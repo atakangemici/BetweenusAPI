6588dcb7254a9659d9073c9516e3f5e59e9d86d8
Atakan Gemici <atakan.gemici@ofisim.com>
2021-01-10 19:05:25 +0300

    bugfix.

--- server.js
+++ server.js
@@ -1,15 +1,22 @@
 var app = require('express')();
 var http = require('http').createServer(app);
 const socketio = require('socket.io');
-const io = socketio(http, 
-  {
-    serveClient: true,
-    cors: {
-      origin: "http://localhost:4200",
-      methods: ["GET", "POST"],
-      credentials: true
-    }
-});
+const io = socketio(http,
+    //   {
+    //     serveClient: true,
+    //     cors: {
+    //       origin: "http://localhost:4200",
+    //       methods: ["GET", "POST"],
+    //       credentials: true
+    //     }
+    {
+        serveClient: true,
+        cors: {
+            origin: "https://chat-angular-new.herokuapp.com",
+            methods: ["GET", "POST"],
+            credentials: true
+        }
+    });
 
 
 
@@ -17,14 +24,20 @@ io.on('connection', (socket) => {
     socket.on('message', (msg) => {
         console.log(msg);
         socket.broadcast.emit('message-broadcast', msg);
-       });
+    });
     console.log('a user connected');
-  });
+});
+
+// app.get('/', (req, res) => res.send('hello!'));
+// http.listen(3000, () => {
+//     console.log('listening on *:3000');
+// });
 
 app.get('/', (req, res) => res.send('hello!'));
-  http.listen(3000 || process.env.PORT, () => {
-  console.log('listening on *:3000');
+http.listen(process.env.PORT, () => {
+    console.log('listening on *:3000');
 });
+
 
 
 
