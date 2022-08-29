2a2b610b06412489bd9cf6bf4c892504067ed576
Atakan Gemici <atakan.gemici@primeapps.io>
2022-08-29 23:20:03 +0300

    prod

--- server.js
+++ server.js
@@ -3,24 +3,24 @@ var http = require('http').createServer(app);
 const socketio = require('socket.io');
 const io = socketio(http,
     //Local
-    {
-        serveClient: true,
-        cors: {
-            origin: "http://localhost:4200",
-            methods: ["GET", "POST"],
-            credentials: true
-        }
-    });
+    // {
+    //     serveClient: true,
+    //     cors: {
+    //         origin: "http://localhost:4200",
+    //         methods: ["GET", "POST"],
+    //         credentials: true
+    //     }
+    // });
 
-//Prod
-// {
-//     serveClient: true,
-//     cors: {
-//         origin: "https://aramizda-app.herokuapp.com",
-//         methods: ["GET", "POST"],
-//         credentials: true
-//     }
-// });
+Prod
+{
+    serveClient: true,
+    cors: {
+        origin: "https://aramizda-app.herokuapp.com",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+});
 
 io.on('connection', (socket) => {
     socket.on('message', (msg) => {
@@ -30,16 +30,16 @@ io.on('connection', (socket) => {
 });
 
 //Local
-app.get('/', (req, res) => res.send('hello!'));
-http.listen(3000, () => {
-    console.log('listening on *:3000');
-});
-
-//Prod
 // app.get('/', (req, res) => res.send('hello!'));
-// http.listen(process.env.PORT, () => {
+// http.listen(3000, () => {
 //     console.log('listening on *:3000');
 // });
+
+//Prod
+app.get('/', (req, res) => res.send('hello!'));
+http.listen(process.env.PORT, () => {
+    console.log('listening on *:3000');
+});
 
 
 
